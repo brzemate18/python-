a1355a45933f5a2155e6b29697067edb704d19de
João Victor Silva <brzemate18@gmail.com>
2026-03-31 13:48:02 -0300

    Add files via upload

---
+++ senha.py
@@ -0,0 +1,8 @@
+senha = input("Digite a senha: ")
+while senha != "1234":
+    print("Senha invalida!")
+    senha = input("Digite a senha novamente: ")
+print("Acesso liberado")
+while tentativas <=3:
+    print("tentativas erradas")
+
